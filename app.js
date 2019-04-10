7b7d891f27d9d70373e4f54d1e70d5927ba4bd01
viktorsbergholcs <v.bergholcs@gmail.com>
2019-04-10 02:07:00 +0200

    fixed port v2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.send("HELLO1");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
